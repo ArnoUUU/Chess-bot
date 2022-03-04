7f3afc75b7884f0d077c430dba8c7e7c4065027f
Arnav Jain <90660802+ArnoUUU@users.noreply.github.com>
2022-03-04 23:11:12 +0000

    feat: added more imports and made a new file for the engine

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import numpy as np
 import math
-from keras import *
-#import tensorflow
+import pygame as pg
+from keras import * 
+import tensorflow
 #won't work on repl, as the version of python is outdated
+from pip import *
+from pip import chess
+# https://python-chess.readthedocs.io/en/latest/pgn.html#writing
+
+# https://levelup.gitconnected.com/chess-python-ca4532c7f5a4
+
+def eval():
+  
+
+
+
+
